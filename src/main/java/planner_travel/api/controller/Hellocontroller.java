fc0c5a9307c91b433e1da5f1d8b54d335802e8a6
Micael Fone <micaelfone0@gmail.com>
2024-08-15 22:09:31 -0300

    adicionando o endpoint de medico

--- src/main/java/planner_travel/api/controller/Hellocontroller.java
+++ src/main/java/planner_travel/api/controller/Hellocontroller.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Hellocontroller {
    @GetMapping
     public String HelloWord(){
-       return "Hello Word";
+       return "Hello Word!";
     }
 }
