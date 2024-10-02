0d011a16108bb8dae38257ce6b02b5836b4f9c0c
Micael Fone <micaelfone0@gmail.com>
2024-10-02 06:59:25 -0300

    Adicionando denotações de descrição da APi para o swagger

--- src/main/java/planner_travel/api/controller/Medicoscontroller.java
+++ src/main/java/planner_travel/api/controller/Medicoscontroller.java
@@ -1,40 +1,46 @@
 package planner_travel.api.controller;
-import jakarta.transaction.Transactional;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import planner_travel.api.DTO.DadosCadastrarMedico;
-import planner_travel.api.medicos.Medico;
-import planner_travel.api.medicos.MedicoRepository;
-import planner_travel.api.DTO.DadosAtualizarMedico;
+import planner_travel.api.DTO.medico.DadosCadastrarMedico;
+import planner_travel.api.DTO.medico.DadosAtualizarMedico;
+import planner_travel.api.service.MedicosService;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
+@Tag(name="Medico",description="API de Medico")
 public class Medicoscontroller {
 
     @Autowired
-    private MedicoRepository repository;
+    private MedicosService service;
     @PostMapping
-    @Transactional
+    @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Cadastrar medico",description ="Descrição de medico")
     public void cadastrar(@RequestBody @Valid DadosCadastrarMedico dados){
-        repository.save(new Medico(dados));
+       service.cadastrar(dados);
     }
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Listar medicos",description ="Lista todos médicos")
     public List<DadosCadastrarMedico> listar() {
-        return repository.findAll().stream().map(DadosCadastrarMedico::new).toList();
+        return service.listar();
     }
-    @PutMapping
-    @Transactional
-    public void atualizar(@RequestBody @Valid DadosAtualizarMedico dados){
-        Medico medico = repository.getReferenceById(dados.id());
-        medico.atualizar(dados);
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Atualiza medicos",description ="Apenas nome,telefone,enderenco e especialidade são atualizados")
+    public void atualizar(@RequestBody @Valid DadosAtualizarMedico dados, @PathVariable long id){
+        service.atualizar(dados,id);
     }
     @DeleteMapping("/{id}")
-    @Transactional
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(summary = "Deletar paciente" ,description = "Através do Id, paciente é removido")
     public void deletar(@PathVariable Long id){
-        repository.deleteById(id);
+        service.deletar(id);
     }
 
 }
