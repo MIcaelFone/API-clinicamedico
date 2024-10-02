b386e6715c0d4bc92f7ff2b0b30bf31dcb890673
Micael Fone <micaelfone0@gmail.com>
2024-10-02 06:48:38 -0300

    Adicionando denotações para não serem vazios os dados para atualizar

--- src/main/java/planner_travel/api/DTO/DadosAtualizarMedico.java
+++
@@ -1,6 +0,0 @@
-package planner_travel.api.DTO;
-
-import planner_travel.api.medicos.Especialidade;
-
-public record DadosAtualizarMedico(long id, String nome, String crm, DadosEndereco endereco, Especialidade especialidade) {
-}
