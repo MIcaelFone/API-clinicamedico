c09f6a77780c78672e167db889fe184b8326b3f4
Micael Fone <micaelfone0@gmail.com>
2024-10-02 06:55:43 -0300

    Model de enderenco

--- src/main/java/planner_travel/api/models/enderenco/Enderenco.java
+++ src/main/java/planner_travel/api/models/enderenco/Enderenco.java
@@ -1,25 +1,32 @@
-package planner_travel.api.enderenco;
+package planner_travel.api.models.enderenco;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import planner_travel.api.DTO.DadosEndereco;
+import planner_travel.api.DTO.Enderenco.DadosEndereco;
 @Embeddable
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Enderenco {
-
+    @Column(name = "logradouro")
     private String logradouro;
+    @Column(name = "cidade")
     private String cidade;
+    @Column(name = "uf")
     private String uf;
+    @Column(name = "pais")
+    private String pais;
+
 
     public Enderenco(@Valid  DadosEndereco dados){
           this.logradouro=dados.logradouro();
           this.cidade= dados.cidade();
           this.uf=dados.uf();
+          this.pais=dados.pais();
     }
 
 }
