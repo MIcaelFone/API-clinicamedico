7b93f828add7e5e698dfa6d583595e950f5458c1
Micael Fone <micaelfone0@gmail.com>
2024-10-17 20:46:51 -0300

    Criando DTO para cadastrar médicos

--- src/main/java/planner_travel/api/DTO/medico/DadosCadastrarMedico.java
+++ src/main/java/planner_travel/api/DTO/medico/DadosCadastrarMedico.java
@@ -2,7 +2,7 @@ package planner_travel.api.DTO.medico;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import planner_travel.api.models.enderenco.Enderenco;
+import planner_travel.api.models.enderenco.EnderencoModel;
 import planner_travel.api.Enum.Especialidade;
 import planner_travel.api.models.medicos.MedicoModel;
 
@@ -15,7 +15,7 @@ public record DadosCadastrarMedico(
         String cpf,
         @NotNull
         Especialidade especialidade,
-        Enderenco enderenco) {
+        EnderencoModel enderenco) {
     public DadosCadastrarMedico(MedicoModel medico) {
         this(medico.getNome(),medico.getCrm(),medico.getCpf(),medico.getEspecialidade(),medico.getEndereco());
     }
