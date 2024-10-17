a284e2e2b8ac026b87e3b66cdff04f9f0858344c
Micael Fone <micaelfone0@gmail.com>
2024-10-17 20:47:10 -0300

    Criando DTO para cadastrar uma consulta

--- src/main/java/planner_travel/api/DTO/Consulta/DadosCadastrarConsulta.java
+++ src/main/java/planner_travel/api/DTO/Consulta/DadosCadastrarConsulta.java
@@ -2,21 +2,27 @@ package planner_travel.api.DTO.Consulta;
 
 import jakarta.validation.constraints.NotNull;
 import planner_travel.api.models.Consulta.ConsultaModel;
-import planner_travel.api.models.medicos.MedicoModel;
-import planner_travel.api.models.paciente.PacienteModel;
 
+import java.sql.Time;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Date;
+import java.util.Timer;
 
 public record DadosCadastrarConsulta(
         @NotNull
-        PacienteModel pacienteModel,
+        Long id_pacienteModel,
 
-        MedicoModel medicoModel,
+        Long id_medicoModel,
         @NotNull
-        LocalDateTime data_consulta) {
+        LocalDate data_consulta,
+        @NotNull
+        LocalTime hora_consulta
+        ) {
 
     public DadosCadastrarConsulta(ConsultaModel consultaModel){
-        this(consultaModel.getPacienteModel(),consultaModel.getMedicoModel(),consultaModel.getData_consulta());
+        this(consultaModel.getPacienteModel(),consultaModel.getIdMedico(),consultaModel.getData_consulta(),consultaModel.getHora_consulta());
     }
 }
 
