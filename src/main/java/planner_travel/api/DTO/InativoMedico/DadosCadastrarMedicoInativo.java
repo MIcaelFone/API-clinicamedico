a249419143e480dc88cc8267556b6aabb6eb8916
Micael Fone <micaelfone0@gmail.com>
2024-10-17 20:46:20 -0300

    Criando DTO de medicos inativos

---
+++ src/main/java/planner_travel/api/DTO/InativoMedico/DadosCadastrarMedicoInativo.java
@@ -0,0 +1,13 @@
+package planner_travel.api.DTO.InativoMedico;
+
+import jakarta.validation.constraints.NotNull;
+import planner_travel.api.models.InativoMedico.InativoMedicoModel;
+
+public record DadosCadastrarMedicoInativo(
+        @NotNull
+        Long id_medico
+) {
+    public DadosCadastrarMedicoInativo(InativoMedicoModel model){
+        this(model.getMedicoModel().getId());
+    }
+}
