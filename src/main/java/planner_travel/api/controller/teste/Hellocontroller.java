117b081aa18760ba047f21e6e3abed672d292d60
Micael Fone <micaelfone0@gmail.com>
2024-10-17 20:40:05 -0300

    Criando controller teste

--- src/main/java/planner_travel/api/controller/teste/Hellocontroller.java
+++ src/main/java/planner_travel/api/controller/teste/Hellocontroller.java
@@ -1,4 +1,4 @@
-package planner_travel.api.controller;
+package planner_travel.api.controller.teste;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
