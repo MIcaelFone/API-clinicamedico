63ad3a9b1be67fdfb502397d7fde5f7c37d25874
Micael Fone <micaelfone0@gmail.com>
2024-10-17 21:49:16 -0300

    Criando service pacientes

--- src/main/java/planner_travel/api/service/Paciente/PacientesService.java
+++ src/main/java/planner_travel/api/service/Paciente/PacientesService.java
@@ -1,11 +1,11 @@
-package planner_travel.api.service;
+package planner_travel.api.service.Paciente;
 
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import planner_travel.api.DTO.paciente.DadosAtualizarPaciente;
 import planner_travel.api.DTO.paciente.DadosCadastrarPaciente;
-import planner_travel.api.Repository.PacienteRepository;
+import planner_travel.api.Repository.Paciente.PacienteRepository;
 import planner_travel.api.models.paciente.PacienteModel;
 
 import java.util.List;
