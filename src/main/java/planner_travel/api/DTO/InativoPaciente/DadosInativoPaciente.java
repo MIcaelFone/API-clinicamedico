e5c6da6388925eb26687bafd83caef8ac248c0c8
Micael Fone <micaelfone0@gmail.com>
2024-10-17 21:55:41 -0300

    Criando  dto de dados inativos de paciente

---
+++ src/main/java/planner_travel/api/DTO/InativoPaciente/DadosInativoPaciente.java
@@ -0,0 +1,13 @@
+package planner_travel.api.DTO.InativoPaciente;
+
+import jakarta.validation.constraints.NotNull;
+import planner_travel.api.models.InativoPaciente.InativoPacienteModel;
+
+public record DadosInativoPaciente(
+        @NotNull
+        Long id_Paciente
+) {
+    public DadosInativoPaciente(InativoPacienteModel model){
+        this(model.getPacienteModel().getId());
+    }
+}
