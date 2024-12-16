cb666ed3c28f4968cc9ea67c661753643b8060b9
Micael Fone <micaelfone0@gmail.com>
2024-12-16 19:48:28 -0300

    Repositorio de médico

--- src/main/java/planner_travel/api/Repository/Medico/MedicoRepository.java
+++ src/main/java/planner_travel/api/Repository/Medico/MedicoRepository.java
@@ -1,7 +1,8 @@
 package planner_travel.api.Repository.Medico;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import planner_travel.api.models.medicos.MedicoModel;
-
+@Repository
 public interface MedicoRepository extends JpaRepository<MedicoModel,Long> {
 }
