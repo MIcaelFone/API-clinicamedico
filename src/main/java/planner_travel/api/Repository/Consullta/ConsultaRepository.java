7b83f8d7911f15af82c2046d18906351167d1cc4
Micael Fone <micaelfone0@gmail.com>
2024-12-16 19:15:05 -0300

    Criando repository da consulta

--- src/main/java/planner_travel/api/Repository/Consullta/ConsultaRepository.java
+++ src/main/java/planner_travel/api/Repository/Consullta/ConsultaRepository.java
@@ -1,7 +1,20 @@
 package planner_travel.api.Repository.Consullta;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import planner_travel.api.DTO.Consulta.MostrarHorariosConsultasDTO;
 import planner_travel.api.models.Consulta.ConsultaModel;
+import planner_travel.api.models.paciente.PacienteModel;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
 
 public interface ConsultaRepository extends JpaRepository<ConsultaModel,Long> {
+    boolean existsByPacienteModelAndDataConsulta(Long id, LocalDate dataConsulta);
+    boolean existsByidMedico(Long idMedico);
+    boolean existsByIdMedicoAndHoraConsultaInicioAndDataConsulta(Long idMedico, LocalTime horaConsulta, LocalDate dataConsulta);
+
+    List <ConsultaModel> findHoraConsultaInicioByIdMedicoAndDataConsulta(Long idMedico,LocalDate diaConsulta);
 }
+
+
