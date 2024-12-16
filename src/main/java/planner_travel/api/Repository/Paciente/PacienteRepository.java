f43f0b8fdc51e5983ed06565d4004b3da2a93eaf
Micael Fone <micaelfone0@gmail.com>
2024-12-16 19:56:20 -0300

    Repositorio de paciente

--- src/main/java/planner_travel/api/Repository/Paciente/PacienteRepository.java
+++ src/main/java/planner_travel/api/Repository/Paciente/PacienteRepository.java
@@ -1,7 +1,8 @@
 package planner_travel.api.Repository.Paciente;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import planner_travel.api.models.paciente.PacienteModel;
-
+@Repository
 public interface PacienteRepository extends JpaRepository<PacienteModel, Long> {
 }
