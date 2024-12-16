a43710c999838ee9fbf2a16f2cafdb266896a169
Micael Fone <micaelfone0@gmail.com>
2024-12-16 19:14:36 -0300

    Finalizando o model da consulta

--- src/main/java/planner_travel/api/models/Consulta/ConsultaModel.java
+++ src/main/java/planner_travel/api/models/Consulta/ConsultaModel.java
@@ -4,9 +4,9 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
+
 import planner_travel.api.DTO.Consulta.DadosCadastrarConsulta;
-import planner_travel.api.models.medicos.MedicoModel;
-import planner_travel.api.models.paciente.PacienteModel;
+
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -16,28 +16,38 @@ import java.time.LocalTime;
 @Table(name = "consulta")
 @AllArgsConstructor
 @NoArgsConstructor
-@EqualsAndHashCode(of="id")
 @Data
 public class ConsultaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "id_paciente")
 
-    private  Long pacienteModel;
+    @Column(name = "id_paciente")
+    private Long pacienteModel;
+
     @Column(name = "id_medico")
     private Long idMedico;
+
     @Column(name = "data_consulta")
-    private LocalDate data_consulta;
-    @Column(name = "hora_consulta")
+    private LocalDate dataConsulta;
+
+    @Column(name = "hora_consulta_inicio")
+    private LocalTime horaConsultaInicio;
+
+    @Column(name = "hora_consulta_final")
     @JsonFormat(pattern = "HH:mm:ss")
-    private LocalTime hora_consulta;
+    private LocalTime horaConsultaFinal;
+    @Column(name = "id_cancela")
+    private Long idConsultaCancelada;
 
     public ConsultaModel(DadosCadastrarConsulta dados) {
-        this.idMedico=dados.id_medicoModel();
-        this.pacienteModel=dados.id_pacienteModel();
-        this.hora_consulta= dados.hora_consulta();
-        this.data_consulta=dados.data_consulta();
+        this.idMedico = dados.id_medico();
+        this.pacienteModel = dados.id_paciente();
+        this.horaConsultaInicio = dados.hora_consulta_inicio();
+        this.horaConsultaFinal = (dados.hora_consulta_inicio() != null) ? dados.hora_consulta_inicio().plusHours(1) : null;
+        this.dataConsulta = dados.data_consulta();
     }
-
+    public void consultaCancelada(Long id){
+          this.idConsultaCancelada=id;
+    }
 }
