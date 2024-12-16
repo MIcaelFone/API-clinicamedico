59811b62039ae99ccf83ff4b4eadc2e23b1eb662
Micael Fone <micaelfone0@gmail.com>
2024-12-16 19:26:56 -0300

    DTO para atualizar paciente

--- src/main/java/planner_travel/api/DTO/paciente/DadosAtualizarPaciente.java
+++ src/main/java/planner_travel/api/DTO/paciente/DadosAtualizarPaciente.java
@@ -5,8 +5,8 @@ import planner_travel.api.DTO.Enderenco.DadosEndereco;
 public record DadosAtualizarPaciente(
         long id,
         String nome,
-        String email,
+
         String telefone,
-        String CPF,
+
         DadosEndereco endereco) {
 }
