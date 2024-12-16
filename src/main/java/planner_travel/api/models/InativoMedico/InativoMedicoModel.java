79e748f9df15b56728f4337dedbc26e925d5a84f
Micael Fone <micaelfone0@gmail.com>
2024-12-16 19:30:34 -0300

    Model de inativo medico

--- src/main/java/planner_travel/api/models/InativoMedico/InativoMedicoModel.java
+++ src/main/java/planner_travel/api/models/InativoMedico/InativoMedicoModel.java
@@ -2,21 +2,27 @@ package planner_travel.api.models.InativoMedico;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import planner_travel.api.DTO.InativoMedico.DadosCadastrarMedicoInativo;
 import planner_travel.api.models.medicos.MedicoModel;
 
-@Table(name = "Inativomedicos")
-@Entity(name = "inativoMedicos")
+@Table(name = "Inativosmedicos")
+@Entity(name = "inativosMedicos")
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-
+@Data
 public class InativoMedicoModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public long id;
-    @ManyToOne
-    @JoinColumn(name = "medico_id",nullable = false)
-    public MedicoModel medicoModel;
+    @Column(name = "id_medico",nullable = false)
+    public Long medicoModel;
+
+    public InativoMedicoModel(DadosCadastrarMedicoInativo dados) {
+
+        this.medicoModel=dados.id_medico();
+    }
 }
