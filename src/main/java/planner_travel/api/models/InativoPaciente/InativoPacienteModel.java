12ee508c9630760dd31d25cb4eb8b0fde8827669
Micael Fone <micaelfone0@gmail.com>
2024-12-16 19:49:15 -0300

    Definição do modelo de pacientes

--- src/main/java/planner_travel/api/models/InativoPaciente/InativoPacienteModel.java
+++ src/main/java/planner_travel/api/models/InativoPaciente/InativoPacienteModel.java
@@ -1,21 +1,24 @@
 package planner_travel.api.models.InativoPaciente;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import planner_travel.api.DTO.InativoPaciente.DadosInativoPaciente;
 import planner_travel.api.models.paciente.PacienteModel;
-@Table(name = "inativoPaciente")
+@Table(name = "inativospaciente")
 @Entity(name = "inativoPaciente")
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Setter
+@Data
 public class InativoPacienteModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public long id;
-    @ManyToOne
-    @JoinColumn(name = "id_paciente",nullable = false)
-    public PacienteModel pacienteModel;
+    @Column(name = "id_paciente",nullable = false)
+    public Long pacienteModel;
 
+    public InativoPacienteModel(DadosInativoPaciente dadosInativoPaciente) {
+        this.pacienteModel = dadosInativoPaciente.id_Paciente();
+    }
 }
